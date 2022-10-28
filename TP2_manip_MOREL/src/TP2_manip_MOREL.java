989fa3efd928f600759e2667b318a53448ef62e8
lea <lea.morel@epfedu.fr>
2022-10-28 16:45:27 +0200

    debut partie 3 : creation de 2 classes

---
+++ TP2_manip_MOREL/src/TP2_manip_MOREL.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author leamo
+ */
+public class TP2_manip_MOREL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ;
+
+    }
+    
+}
