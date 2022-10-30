217393af129a0e941d59b4a10783366e8d4160ba
lea <lea.morel@epfedu.fr>
2022-10-30 21:00:13 +0100

    fin TP2

--- TP2_relation_1_MOREL/src/tp2_relation_1_morel/TP2_relation_1_MOREL.java
+++ TP2_relation_1_MOREL/src/tp2_relation_1_morel/TP2_relation_1_MOREL.java
@@ -21,32 +21,32 @@ public class TP2_relation_1_MOREL {
         Voiture uneMicra = new Voiture("Micra", "Nissan", 4);
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
-        System.out.println("liste des voitures disponibles " + uneClio  + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra);
-        
-        bob.liste_voitures[0] = uneClio ;
-        bob.nbVoitures = 1 ;
-        uneClio.proprietaire = bob ;
-        
-        System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0] ) ;
-        
+        System.out.println("liste des voitures disponibles " + uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra);
+
+        bob.liste_voitures[0] = uneClio;
+        bob.nbVoitures = 1;
+        uneClio.proprietaire = bob;
+
+        System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0]);
+
         bob.liste_voitures[1] = une2008;
         bob.nbVoitures = 2;
         une2008.proprietaire = bob;
-        
-        System.out.println("les voitures de bob sont " + bob.liste_voitures[0] + " et " + bob.liste_voitures[1] );
-        
-        reno.liste_voitures[0]=uneMicra;
-        reno.liste_voitures[1]=uneAutreClio ;
+
+        System.out.println("les voitures de bob sont " + bob.liste_voitures[0] + " et " + bob.liste_voitures[1]);
+
+        reno.liste_voitures[0] = uneMicra;
+        reno.liste_voitures[1] = uneAutreClio;
         reno.nbVoitures = 2;
         uneMicra.proprietaire = reno;
-        uneAutreClio.proprietaire=reno;
-        
-        System.out.println("les voitures de reno sont " + reno.liste_voitures[0] + " et " + reno.liste_voitures[1] );
-        
+        uneAutreClio.proprietaire = reno;
+
+        System.out.println("les voitures de reno sont " + reno.liste_voitures[0] + " et " + reno.liste_voitures[1]);
+
         System.out.println(bob.nbVoitures);
         boolean ajouter_voiture = bob.ajouter_voiture(uneMicra);
         System.out.println(bob.nbVoitures);
-                
+
     }
 
 }
